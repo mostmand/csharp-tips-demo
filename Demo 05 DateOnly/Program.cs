e2f71ad09630f13b1bb41065123071334c8533d5
mostmand <mostmand.mh@gmail.com>
2022-03-11 15:16:14 +0330

    Add demo 5

---
+++ Demo 05 DateOnly/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_05_DateOnly;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var dateOnly = DateOnly.FromDateTime(DateTime.Now.Date);
+        Console.WriteLine(dateOnly);
+    }
+}
