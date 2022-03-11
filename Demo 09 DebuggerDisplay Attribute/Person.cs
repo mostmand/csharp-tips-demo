3567aa28d325fffdf36e5f0db42152f8fede2e41
mostmand <mostmand.mh@gmail.com>
2022-03-11 15:59:51 +0330

    Add demo 9

---
+++ Demo 09 DebuggerDisplay Attribute/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace Demo_09_DebuggerDisplay_Attribute;
+
+[DebuggerDisplay("{Name}-{LastName}")]
+internal class Person
+{
+    public string Name { get; init; }
+    public string LastName { get; init; }
+}
---
+++ Demo 09 DebuggerDisplay Attribute/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_09_DebuggerDisplay_Attribute;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var person = new Person()
+        {
+            Name = "Mohammad Hossein",
+            LastName = "Mostmand"
+        };
+
+        Console.WriteLine("Done");
+    }
+}
