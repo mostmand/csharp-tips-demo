73a840a86665a9dafaccc5bb2bb924a768996869
mostmand <mostmand.mh@gmail.com>
2022-03-11 15:14:10 +0330

    Add demo 4

---
+++ Demo 04 Conditional/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Demo_04_Conditional;
+
+internal static class Program
+{
+    private static bool _debugMode = false;
+
+    public static void Main()
+    {
+        A();
+        B();
+        Console.WriteLine(_debugMode);
+    }
+
+    private static void A()
+    {
+#if DEBUG
+        _debugMode = true;
+#endif
+    }
+
+    [Conditional("DEBUG")]
+    private static void B()
+    {
+        _debugMode = true;
+    }
+}
