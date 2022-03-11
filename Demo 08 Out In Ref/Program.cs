1608af1c2a4c65ceca1895bd35cad1c3d41cc06b
mostmand <mostmand.mh@gmail.com>
2022-03-11 15:54:50 +0330

    Add demo 8

---
+++ Demo 08 Out In Ref/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo_08_Out_In_Ref;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var dictionary = new Dictionary<string, string> { { "A", "Salam" } };
+        dictionary.TryGetValue("A", out var value);
+        Console.WriteLine(value);
+    }
+
+    private static void InExample(in char character)
+    {
+        if (character == 'a')
+        {
+            Console.WriteLine("It's \"a\"!");
+        }
+    }
+
+    private static void RefExample(ref char character)
+    {
+        if (character == 'a')
+        {
+            Console.WriteLine("It's \"a\"!");
+            character = 'z';
+        }
+    }
+
+    private static void OutExample(out char character)
+    {
+        character = 'z';
+    }
+}
