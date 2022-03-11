0dd6a42dd4b263fa83a6b934cc1e0b54f181be21
mostmand <mostmand.mh@gmail.com>
2022-03-11 15:06:17 +0330

    Add demo 2

---
+++ Demo 02 Init/Foo.cs
@@ -0,0 +1,7 @@
+﻿namespace Demo_02_Init;
+
+public class Foo
+{
+    public string A { get; set; }
+    public string B { get; init; }
+}
---
+++ Demo 02 Init/Program.cs
@@ -0,0 +1,7 @@
+﻿using Demo_02_Init;
+
+var foo = new Foo();
+foo.A = "Salam";
+
+// Error
+// foo.B = "Salam";
