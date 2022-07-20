bc879f1fd27015ebb608864e3e8b62a7d27e87f8
mostmand <mostmand.mh@gmail.com>
2022-07-20 15:35:42 +0430

    Add OneOf tip

---
+++ Demo 15 OneOf/Program.cs
@@ -0,0 +1,16 @@
+﻿using Demo_15_OneOf;
+
+var sample = new Sample();
+
+try
+{
+    var result = sample.DoAndThrowExceptionOnError();
+    Console.WriteLine(result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+sample.DoAndReturnResultOrError().Switch(result => { Console.WriteLine(result); },
+    ex => { Console.WriteLine(ex); });
