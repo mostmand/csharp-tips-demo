61ea2ff3ae40baf581ee5ce11c7554f113b9c559
mostmand <mostmand.mh@gmail.com>
2022-07-20 10:47:56 +0430

    Add empty collections tip

---
+++ Demo 11 Empty Collections/Samples.cs
@@ -0,0 +1,24 @@
+﻿namespace Demo_11_Empty_Collections;
+
+public class Samples
+{
+    public List<int> NewList()
+    {
+        return new List<int>();
+    }
+    
+    public IEnumerable<int> EnumerableEmpty()
+    {
+        return Enumerable.Empty<int>();
+    }
+    
+    public int[] NewArray()
+    {
+        return new int[] { };
+    }
+    
+    public int[] ArrayEmpty()
+    {
+        return Array.Empty<int>();
+    }
+}
