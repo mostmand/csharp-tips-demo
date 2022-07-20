42eb58379c8dd04c47584158a5d218b1ca9e685d
mostmand <mostmand.mh@gmail.com>
2022-07-20 15:35:42 +0430

    Add OneOf tip

---
+++ Demo 15 OneOf/Sample.cs
@@ -0,0 +1,35 @@
+﻿using OneOf;
+
+namespace Demo_15_OneOf;
+
+public class Sample
+{
+    private readonly Random _random = new();
+    
+    public int DoAndThrowExceptionOnError()
+    {
+        if (ThrowError())
+        {
+            throw new Exception("Normal Exception");
+        }
+
+        return 10;
+    }
+    
+    public OneOf<int, Exception> DoAndReturnResultOrError()
+    {
+        if (ThrowError())
+        {
+            return new Exception("OneOf Error");
+        }
+
+        return 10;
+    }
+
+    private bool ThrowError()
+    {
+        var randomNumber = _random.Next(0, 2);
+        var throwError = randomNumber == 1;
+        return throwError;
+    }
+}
