582fe6ea626d92300373b5d86c7ca5883418707f
mostmand <mostmand.mh@gmail.com>
2022-07-20 13:28:55 +0430

    Add ArrayPool tip

---
+++ Demo 12 ArrayPool.Shared/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+
+namespace Demo_12_ArrayPool.Shared;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private const int BufferSize = 2 * 1024;
+    
+    [Benchmark]
+    public int CreateArray()
+    {
+        var sizeSum = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            var bytes = new byte[BufferSize];
+            sizeSum += bytes.Length;
+        }
+
+        return sizeSum;
+    }
+
+    [Benchmark]
+    public int ArrayPool()
+    {
+        var sizeSum = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            var bytes = ArrayPool<byte>.Shared.Rent(BufferSize);
+            sizeSum += bytes.Length;
+            ArrayPool<byte>.Shared.Return(bytes);
+        }
+
+        return sizeSum;
+    }
+    
+    
+}
