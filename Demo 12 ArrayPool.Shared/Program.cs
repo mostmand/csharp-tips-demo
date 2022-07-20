f370bca01754b0149d1e45cf5d58d749a50c5e2c
mostmand <mostmand.mh@gmail.com>
2022-07-20 13:28:55 +0430

    Add ArrayPool tip

---
+++ Demo 12 ArrayPool.Shared/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using BenchmarkDotNet.Running;
+using Demo_12_ArrayPool.Shared;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+// const string filePath = "Resources/content.txt";
+// const int bufferSize = 2 * 1024;
+//
+// async Task<string> ReadToEndAsync(byte[] buffer)
+// {
+//     await using var fileStream = File.OpenRead(filePath);
+//     var result = new StringBuilder();
+//     int readBytes;
+//     while ((readBytes = await fileStream.ReadAsync(buffer)) > 0)
+//     {
+//         result.Append(Encoding.UTF8.GetString(buffer[..readBytes]));
+//     }
+//
+//     return result.ToString();
+// }
+//
+// var buffer = new byte[bufferSize];
+// var result = await ReadToEndAsync(buffer);
